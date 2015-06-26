ded53134986b24906aee93ab6c8f52a7750eaea6
Julie Kuehl <julie@juliekuehl.com>
2015-06-25 19:03:13 -0500

    main nav placement

--- themes/woody/header.php
+++ themes/woody/header.php
@@ -31,7 +31,9 @@
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'woody' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			<div class="boxed">
+				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+			</div><!-- .boxed -->
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
